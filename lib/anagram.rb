a2ebb7fa6dcbc68fa03a60883d692a2df426f3ee
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-10 04:00:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,3 @@
 # Your code goes here!
 class Anagram
   
-attr_accessor :word
- 
- def initialize()
-  
- end
-
-def match(array_of_words)
-  array_of_words.select do |element|(@word.split(" ").sort == (element.split(" ").sort))
-    end
-  end
-end
-
-listen = Anagram.new("listen")
-listen.match(%w(enlists google inlets banana))
